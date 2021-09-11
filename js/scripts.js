6dfc6d2581bee3553cd247cd8a943d63396b6118
r-gaston <rgaston001@columbus.rr.com>
2021-09-11 10:06:15 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,15 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
+    });    
+
+    $('#reserveButton').click(function(){          
+        $('#reserveModal').modal(focus);
     });
+
+    $('#loginButton').click(function(){          
+        $('#loginModal').modal(focus);
+    });
+    
+   
 });
